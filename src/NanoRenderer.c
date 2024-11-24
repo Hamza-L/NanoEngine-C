4af5a73691da30d47c3db58a4a49935aef0563b9
Hamza Lahmimsi <h_lahmimsi@maxon.net>
2024-11-24 03:59:35 -0500

    fixed a segfault from a strcpy

--- src/NanoRenderer.c
+++ src/NanoRenderer.c
@@ -251,7 +251,7 @@ void getRequiredInstanceExtensions(char* requiredInstanceExtensions[], uint32_t*
     // 64 additional extensions that we can potentially add
     int extIdx = 0;
     while (*desiredInstanceExtensions[extIdx]){
-        strcpy(requiredInstanceExtensions[extIdx], desiredInstanceExtensions[extIdx]);
+        requiredInstanceExtensions[extIdx] = (char*)desiredInstanceExtensions[extIdx];
         extIdx++;
     }
 
--- src/NanoShader.c
+++ src/NanoShader.c
@@ -29,7 +29,7 @@ static void CreateDescriptorSetLayout(NanoRenderer* nanoRenderer, NanoShader* sh
     uboLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
     uboLayoutBinding.pImmutableSamplers = nullptr; // Optional. used for texture sampling
 
-    VkDescriptorSetLayoutCreateInfo layoutInfo{};
+    VkDescriptorSetLayoutCreateInfo layoutInfo = {};
     layoutInfo.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
     layoutInfo.bindingCount = 1;
     layoutInfo.pBindings = &uboLayoutBinding;
