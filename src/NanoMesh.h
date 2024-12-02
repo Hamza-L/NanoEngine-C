22892422f0c9e9d00e93b2321a4e8dfd212d6a7b
Hamza Lahmimsi <h_lahmimsi@maxon.net>
2024-12-02 02:51:12 +0000

    Added FreeType library. Tested font imports. Compiles and runs with no issues. yet to display font

---
+++ src/NanoMesh.h
@@ -0,0 +1,8 @@
+#ifndef NANOMESH_H_
+#define NANOMESH_H_
+
+typedef struct{
+
+} NanoMesh;
+
+#endif // NANOMESH_H_
--- src/main.c
+++ src/main.c
@@ -12,7 +12,48 @@
 
 #include "NanoConfig.h"
 
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+FT_Library  library;
+FT_Face     face;      /* handle to face object */
+
 int main(int argc, char *argv[]) {
+    int error = FT_Init_FreeType( &library );
+    if ( error )
+    {
+        fprintf(stderr, "ERROR OCCURED LOADING FONTS\n");
+        abort();
+    }
+    error = FT_New_Face( library,
+                         "/Users/h_lahmimsi/Library/Fonts/Cascadia.ttf",
+                         0,
+                         &face );
+
+    if ( error == FT_Err_Unknown_File_Format )
+    {
+        fprintf(stderr, "UNKNOWW FILE FORMAT FOR GIVEN FONTS\n");
+        abort();
+    }
+    else if ( error )
+    {
+        fprintf(stderr, "UNKOWN ERROR OCCURED LOADING FONTS\n");
+        abort();
+    }
+
+    error = FT_Set_Char_Size(
+        face,    /* handle to face object         */
+        0,       /* char_width in 1/64 of points  */
+        16*64,   /* char_height in 1/64 of points */
+        300,     /* horizontal device resolution  */
+        300 );   /* vertical device resolution    */
+
+    if ( error )
+    {
+        fprintf(stderr, "ERROR SETTING FONT SIZE\n");
+        abort();
+    }
+
     SetVar(argv[0]);
     if(argc > 1){
         fprintf(stderr, "argument passed in\n");
