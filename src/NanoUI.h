7c7ade4ea0e67be17efff276809dff84a46ae30e
Hamza Lahmimsi <h_lahmimsi@maxon.net>
2025-01-10 23:25:38 -0500

    Start of custom UI implementation

--- src/NanoUI.h
+++ src/NanoUI.h
@@ -1,6 +1,32 @@
 #ifndef NANOUI_H_
 #define NANOUI_H_
 
+#include "NanoScene.h"
+
+typedef enum UIState UIState;
+typedef struct UIElement UIElement;
+typedef struct NanoUI NanoUI;
+
+enum UIState{
+STATE_DISABLED = 0x00000000,
+STATE_ENABLED = 0x00000001,
+STATE_FOCUSED = 0x00000010,
+STATE_PRESSED = 0x00000100,
+};
+
+struct UIElement{
+    RenderableNode element;
+    UIState state;
+
+    void (*Action)(void* self, void* data);
+};
+
+struct NanoUI{
+    RenderableScene scene;
+
+    UIElement* elements;
+    uint32_t numUIElements;
+};
 
 
 #endif // NANOUI_H_
